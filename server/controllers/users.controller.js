dd1f054932918bc0044dc8e5f1c9eae98fe73fea
Luke Vance <lukevance90@gmail.com>
2016-05-09 10:58:14 -0600

    moved user routes functions to user controller

---
+++ server/controllers/users.controller.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+
+var models = require('../models');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  models.User
+    .all()
+    .then(function(usersList){
+      res.json({users: usersList});
+    });
+});
+
+// Create New User
+router.post('/new', function(req, res, next) {
+  models.User
+    .create({
+      first_name: req.body.firstName,
+      last_name: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password
+    })
+    .then(function(){
+      models.User.findAll()
+        .then(function(allUsers){
+          res.json({users: allUsers});
+        })
+        .catch(function(err){
+          res.json({error: err});
+        });
+    });
+});
+
+module.exports = router;
