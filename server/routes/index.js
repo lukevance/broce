7d01aab46a08aacce44e8e4610cf4aa31bfc5637
Luke Vance <lukevance90@gmail.com>
2016-05-09 10:46:14 -0600

    added routes to basic controller files

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const router = require('express').Router();
+
+// require controllers for all routes
+const auth = require('../controllers/auth.controller');
+const orders = require('../controllers/orders.controller');
+const parts = require('../controllers/parts.controller');
+const users = require('../controllers/users.controller');
+
+
+router.use('/auth', auth);
+router.use('/orders', orders);
+router.use('/parts', parts);
+router.use('/users', users);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
