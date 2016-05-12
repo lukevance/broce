40c8676e33742881a38ea142df1191c463abaa2d
Luke Vance <lukevance90@gmail.com>
2016-05-12 12:40:42 -0500

    added order_details creation route needs to be tested

--- server/routes.js
+++ server/routes.js
@@ -1,19 +1,19 @@
 const router = require('express').Router();
 
 // require controllers for all routes
-// const auth = require('../controllers/auth.controller');
-// const orders = require('../controllers/orders.controller');
+// const auth = require('./controllers/auth.controller');
+const orders = require('./controllers/orders.controller');
 const parts = require('./controllers/parts.controller');
 const users = require('./controllers/users.controller');
 
 // router.use('/auth', auth);
-// router.use('/orders', orders);
+router.use('/orders', orders);
 router.use('/parts', parts);
 router.use('/users', users);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.json({ title: 'Broce Parts Api' });
 });
 
 module.exports = router;
