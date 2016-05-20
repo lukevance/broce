1c6a578033fa682fb8f147f9b55c08f73c990a70
Luke Vance <lukevance90@gmail.com>
2016-05-20 10:48:11 -0600

    added migration file for order details

---
+++ server/db/orderDetailsMigrations.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = [
+  {
+    machineSerialNum: '300124',
+    quantity: 3,
+    Part: 1,
+    Order: 1
+  },
+  {
+    machineSerialNum: '400567',
+    quantity: 2,
+    Part: 2,
+    Order: 1
+  },
+  {
+    machineSerialNum: '',
+    quantity: 3,
+    Part: 1,
+    Order: 2
+  },
+  {
+    machineSerialNum: '',
+    quantity: 3,
+    Part: 1,
+    Order: 2
+  },
+  {
+    machineSerialNum: '',
+    quantity: 3,
+    Part: 1,
+    Order: 2
+  }
+];
