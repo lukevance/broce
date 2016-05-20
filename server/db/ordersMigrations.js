64f9d277a19deda7c8f8ea7a04d2da235dda586b
Luke Vance <lukevance90@gmail.com>
2016-05-20 10:53:19 -0600

    added migration file for orders

---
+++ server/db/ordersMigrations.js
@@ -0,0 +1,34 @@
+"use strict";
+
+module.exports = {
+  [
+    {
+      shippingAddress: '123 Main St',
+      poNumber: 'UFB1235',
+      total: 123,
+      status: 'quoted',
+      note: "I couldn't find one of these parts, do you guys still make the blah blab blah?"
+    },
+    {
+      shippingAddress:
+      poNumber:
+      total:
+      status:
+      note:
+    },
+    {
+      shippingAddress:
+      poNumber:
+      total:
+      status:
+      note:
+    },
+    {
+      shippingAddress:
+      poNumber:
+      total:
+      status:
+      note:
+    }
+  ]
+}
