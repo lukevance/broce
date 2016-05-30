6beb22911cff929ae37c853cf77d89806275a47a
Jonathan Leathe <Jonny@Jonathans-MacBook-Pro.local>
2016-05-30 11:41:54 -0400

    routes and css issues

---
+++ client/app/routes.js
@@ -0,0 +1,38 @@
+var partsApp = angular.module('partsApp', ['ngRoute']);
+
+    // configure our routes
+  partsApp.config(function($routeProvider) {
+        $routeProvider
+
+            // route for the home page/signin page
+            .when('/', {
+                templateUrl : 'app/pages/signin.html',
+                controller  : 'signinController',
+                css : 'styles.css'
+            })
+
+            // route for the about page
+            .when('/about', {
+                templateUrl : 'pages/about.html',
+                controller  : 'aboutController'
+            })
+            // route for the contact page
+            .when('/contact', {
+                templateUrl : 'pages/contact.html',
+                controller  : 'contactController'
+            });
+    });
+
+    // create the controller and inject Angular's $scope
+    partsApp.controller('signinController', function($scope) {
+        // create a message to display in our view
+        $scope.message = 'SIGN IN';
+    });
+
+    partsApp.controller('aboutController', function($scope) {
+        $scope.message = 'Look! I am an about page.';
+    });
+
+    partsApp.controller('contactController', function($scope) {
+        $scope.message = 'Contact us! JK. This is just a test.';
+    });
