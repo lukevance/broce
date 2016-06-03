addbd8355139d3e3b6e27050a2480df741e1a19c
Luke Vance <lukevance90@gmail.com>
2016-06-03 09:20:32 -0600

    added signin route with hashed password comparison

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
 
 var models = require('../models');
 
@@ -8,55 +9,72 @@ var models = require('../models');
 router.post('/signup', function(req, res){
   // validation/sanitation
   let newUser = req.body;
-  // check for existing entry
-  models.User
-    .findOrCreate({
-      where: {email: newUser.email},
-      defaults: {
-        first_name: req.body.firstName,
-        last_name: req.body.lastName,
-        password: req.body.password
-      }
-    })
-    .spread(function(user, created){
-      if (!created) {
-        res.json({message: 'Sorry there is already an account with that email address.'});
+  // check for matching confirm password to match
+  if (newUser.password !== newUser.confPassword){
+    res.json({message: 'Passwords do not match.'});
+  } else {
+    // encrypt password
+    bcrypt.hash(newUser.password, 8, function(err, hash) {
+      if (err) {
+        return res.json(err);
       } else {
-        res.json(user);
+        // check for existing entry
+        models.User
+          .findOrCreate({
+            where: {email: newUser.email},
+            defaults: {
+              first_name: req.body.firstName,
+              last_name: req.body.lastName,
+              password: hash
+            }
+          })
+          .spread(function(user, created){
+            if (!created) {
+              res.json({message: 'Sorry there is already an account with that email address.'});
+            } else {
+              res.json(user);
+            }
+          })
+          .catch(function(err){
+            res.json({error: err});
+          });
       }
-    })
-    .catch(function(err){
-      res.json({error: err});
     });
-
-
-
-  // // check for matching confirm password to match
-  // models.User
-  //   .create({
-  //     first_name: req.body.firstName,
-  //     last_name: req.body.lastName,
-  //     email: req.body.email,
-  //   })
-  //   .then(function(){
-  //     models.User.findAll()
-  //       .then(function(allUsers){
-  //         res.json({users: allUsers});
-  //       })
-  //       .catch(function(err){
-  //         res.json({error: err});
-  //       });
-  //   });
-
-
+  }
 });
 
 
 
 // Signin
 router.post('/signin', function(req, res){
+  // some sort of sanitation
   // lookup user
-  // check if password matches
+  models.User
+    .findOne({
+      where: {
+        email: req.body.email      }
+    })
+    .then(function(userInfo){
+      if (userInfo) {
+        // check if password matches
+        bcrypt.compare(req.body.password, userInfo.password, function(err, response){
+          if (err) {
+            console.log(err);
+            res.json({error: err});
+          } else {
+            // if passwords match
+            if (response === true) {
+              // send signed token
+              res.json({message: 'you are signed in!', user: userInfo});
+            } else {
+              res.json({message: 'username or password did not match'});
+            }
+          }
+        });
+      } else {
+        res.json({message: 'username or password did not match'});
+      }
+    });
 });
 
 module.exports = router;
