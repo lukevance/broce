ef25323932a60c16d0ab8096129c12eed7fed393
Jonathan Leathe <Jonny@Jonathans-MacBook-Pro.local>
2016-06-03 10:09:32 -0400

    fixing module parse error

--- client/app/app.js
+++ client/app/app.js
@@ -7,10 +7,3 @@ require('./services');
 require('./controllers');
 require('./routes');
 //require('./pages');
-<<<<<<< HEAD
-//require('.css')   //dont know if this should be in here or not,
-                    //I get an error when I do
-=======
-//require('./css');
-//dont know if these are supposed to go here but when I do I get an error...
->>>>>>> d5359bd9367fda7bfd077889695d891b75573d68
---
+++ client/app/controllers/signup.controller.js
@@ -0,0 +1,7 @@
+
+function SignupController () {
+  var vm = this;
+  vm.message = 'yeah it totally works';
+}
+
+module.exports = SignupController;
