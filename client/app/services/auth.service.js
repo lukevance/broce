f8053476e275e7037f7c87411a06a8658652d7c2
Luke Vance <lukevance90@gmail.com>
2016-06-03 09:58:06 -0600

    added functional signin service

--- client/app/services/auth.service.js
+++ client/app/services/auth.service.js
@@ -1,10 +1,27 @@
 'use strict';
 
-// inject htttp service from angular
+// inject http service from angular
 SignupService.$inject = ['$http'];
 function SignupService ($http) {
   var message = 'you are trying to sign up!';
   return message;
 }
 
-module.exports = SignupService;
+function SigninService ($http) {
+  return function (userCredentials, nextFunc) {
+    return $http.post('//localhost:3000/auth/signin', userCredentials)
+    .then(function(authorizedUserData){
+      console.log(authorizedUserData);
+      nextFunc(authorizedUserData);
+    })
+    .catch(function(err){
+      console.log(err);
+      // if necessary delete window storage of token
+    });
+  };
+}
+
+module.exports = {
+  SignupService: SignupService,
+  SigninService: SigninService
+};
