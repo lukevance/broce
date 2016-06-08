77f41823671635e64bd81461e42761fe1737cef2
Jonathan Leathe <Jonny@Jonathans-MacBook-Pro.local>
2016-06-08 11:01:12 -0400

    error checking for signin and signup pages

--- client/app/app.js
+++ client/app/app.js
@@ -2,10 +2,11 @@ angular.module('partsApp', ['ngRoute']);
 
 // include styles from css folder
 require('./css/styles.css');
-
+require('./css/jquery-1.12.4.min');
 // include angular modules
 // require('./directives');
 require('./services');
 require('./controllers');
 require('./routes');
+
 //require('./pages');
--- client/app/controllers/auth.controller.js
+++ client/app/controllers/auth.controller.js
@@ -3,41 +3,47 @@ function AuthController(SigninService, SignupService) {
     function submitSignIn(credentials) {
         console.log('signin');
         SigninService(credentials, afterSignIn);
-
-
-
-
     }
     function afterSignIn(response) {
-
-        console.log('afterSignIn');
+      // var errors = false;
+      // $('#signin').submit(function(event){
+        if(response.data.message !== nope){
+          errors = true;
+          alert('wrong email address or password');
+        }
+      else{
+          console.log('it worked');
+        }
+        console.log(response.data.message);
+        console.log(nope);
+      // });
     }
 
     function submitSignUp(credentials) {
         console.log('submitSignUp');
         SignupService(credentials, afterSignUp);
-
-
     }
 
     function afterSignUp(response) {
+        if(response.config.data.password !== response.config.data.confPassword){
+          alert("Passwords dont match");
+        }else if (response.config.data.email !== response.config.data.confEmail) {
+          alert("Email adresses dont match");
+        }else{
+          console.log("everything checks out");
+        }
         console.log(response);
 
     }
 
-    // function signup(credentials){
-    //   SignupService(credentials);
-    //
-    //
-    // }
-
+    var nope = 'you are signed in!';
 
     var vm = this;
     vm.userInfo = {};
     vm.newUser = {};
     // vm.signup = signup;
     vm.message = 'SIGN IN';
-    
+
     vm.submit = submitSignIn;
     vm.submitSU = submitSignUp
 
