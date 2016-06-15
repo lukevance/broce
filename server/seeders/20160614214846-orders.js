87c5049b6fdf8751f3b93af2896d2a5a1ec68c3b
Luke Vance <lukevance90@gmail.com>
2016-06-15 12:15:51 -0600

    added seed file for order details and changed machine serial num type to integer

--- server/seeders/20160614214846-orders.js
+++ server/seeders/20160614214846-orders.js
@@ -2,16 +2,6 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
     return queryInterface.bulkInsert('Order', [
       {
         shipping_address: '',
---
+++ server/seeders/20160615181016-order_details.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Order_Detail', [
+      {
+        machine_serial_num: 407638,
+        quantity: 2,
+        OrderId: 1,
+        PartId: 1
+      }
+    ]);
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+    return queryInterface.bulkDelete('Order_Detail', null, {});
+  }
+};
