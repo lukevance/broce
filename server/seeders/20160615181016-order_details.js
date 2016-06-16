93fbf171ccba6905231a9bd92925528cf0fb21b5
Luke Vance <lukevance90@gmail.com>
2016-06-15 22:58:59 -0600

    fixed seed files for orders and order_details

--- server/seeders/20160615181016-order_details.js
+++ server/seeders/20160615181016-order_details.js
@@ -2,34 +2,19 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
-    return queryInterface.bulkInsert('Order_Detail', [
+    return queryInterface.bulkInsert('Order_Details', [
       {
         machine_serial_num: 407638,
         quantity: 2,
         OrderId: 1,
-        PartId: 1
+        PartId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
       }
     ]);
   },
 
   down: function (queryInterface, Sequelize) {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('Person', null, {});
-    */
-    return queryInterface.bulkDelete('Order_Detail', null, {});
+    return queryInterface.bulkDelete('Order_Details', null, {});
   }
 };
