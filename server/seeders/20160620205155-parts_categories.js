b4f1ef29ee1ffef144678c9abd40d6cf6a60eba4
Luke Vance <lukevance90@gmail.com>
2016-06-20 20:59:52 -0600

    added table for part_categories joins and seed files

---
+++ server/seeders/20160620205155-parts_categories.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Part_Categories', [
+      {
+        model: '350',
+        serial_min: 408094,
+        serial_max: null,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        model: '350',
+        serial_min: 407638,
+        serial_max: 408093,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]);
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('Part_Categories', null, {});
+  }
+};
