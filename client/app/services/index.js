d5c7c11ab91a76e0e86e2c89e175efebaa7d9630
Luke Vance <lukevance90@gmail.com>
2016-06-22 11:36:07 -0600

    added service to check for token

--- client/app/services/index.js
+++ client/app/services/index.js
@@ -5,4 +5,5 @@ var angular = require('angular');
 angular.module('partsApp')
   .service('SigninService', ['$http', require('./auth.service').SigninService])
   .service('SignupService',['$http', require('./auth.service').SignupService])
+  .service('CheckForAuthService', ['$window', require('./auth.service').CheckForAuthService])
   .service('GetUsersQuotes', ['$http', require('./orders.service')]);
