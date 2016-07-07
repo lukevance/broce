ee44a05eaf81de62710a3eea75bead70f751e149
Luke Vance <lukevance90@gmail.com>
2016-07-07 15:34:48 -0600

    added functionality for orders directive

--- client/app/services/dummyData.service.js
+++ client/app/services/dummyData.service.js
@@ -90,6 +90,41 @@ function adminAllOrders () {
         },
       ],
       orders: [
+        {
+          account: 'The Real DEALer',
+          user: 'Kevin Love',
+          createdAt: new Date(),
+          details: [
+            {
+              machine_serial_num: 304576,
+              parts: [
+                {
+                  number: 'R10798',
+                  quantity: 3,
+                  price: 21.73
+                },
+                {
+                  number: 'AD0976',
+                  quantity: 2,
+                  price: 11.92
+                }
+              ]
+            },
+          ],
+          total: 89.03,
+          comments: [
+            {
+              user: 'Ray Bray',
+              date: new Date(),
+              text: 'Hey there bryan I changed your first part number to the appropriate number for your model. Let me know if you have any questions.'
+            },
+            {
+              user: 'Bryan Buyer',
+              date: new Date(),
+              text: 'Sounds Good Ray, thanks again you guys are the best.'
+            }
+          ]
+        },
 
       ],
       shippedOrders: [
