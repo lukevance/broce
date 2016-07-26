3b638a56994faa387552f0ea76b8e1dcff0c76ac
Luke Vance <lukevance90@gmail.com>
2016-07-26 11:16:54 -0600

    added angular middleware for authenticated requests to server

--- client/app/app.js
+++ client/app/app.js
@@ -1,9 +1,13 @@
-angular.module('partsApp', ['ngRoute']);
+angular.module('partsApp', ['ngRoute'])
+  .config(function ($httpProvider) {
+    $httpProvider.interceptors.push('authInterceptor');
+  });
 
 // include styles from css folder
 require('./css/styles.css');
 require('./css/jquery-1.12.4.min');
 // include angular modules
+require('./factories');
 require('./directives');
 require('./services');
 require('./controllers');
