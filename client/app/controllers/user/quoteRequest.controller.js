146084f711a6a35e9361d01dc03d79b70830e735
Luke Vance <lukevance90@gmail.com>
2016-08-15 14:49:17 -0600

    added form and obj to track order details

--- client/app/controllers/user/quoteRequest.controller.js
+++ client/app/controllers/user/quoteRequest.controller.js
@@ -1,6 +1,28 @@
 function QuoteRequestController() {
   var vm = this;
-  vm.blah = 'blachhh';
-}
+
+  // storage obj for quote details
+  vm.quoteSummary = {};
+
+  // function to add part
+  vm.addPart = addPart;
+  function addPart (serialNum, partNum, partQty) {
+    var partInfo = {
+      machine_serial: serialNum,
+      part_number: partNum,
+      quantity: partQty
+    };
+    if (vm.quoteSummary[serialNum]){
+      vm.quoteSummary[serialNum].push(partInfo);
+    } else {
+      vm.quoteSummary[serialNum] = [];
+      vm.quoteSummary[serialNum].push(partInfo);
+    }
+    vm.partNum = '';
+    vm.partQty = '';
+    console.log(vm.quoteSummary);
+  }
+
+} // end of controller
 
 module.exports = QuoteRequestController;
