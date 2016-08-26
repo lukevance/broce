d40baf7efd4c3363a28a7a3037371580469d426b
Luke Vance <lukevance90@gmail.com>
2016-08-25 21:30:17 -0600

    added quotes to routes file and removed admin

--- server/routes.js
+++ server/routes.js
@@ -2,14 +2,14 @@ const router = require('express').Router();
 
 // require controllers for all routes
 const auth = require('./controllers/auth.controller');
-const admin = require('./controllers/admin.controller');
+const quotes = require('./controllers/quotes.controller');
 const orders = require('./controllers/orders.controller');
 const parts = require('./controllers/parts.controller');
 const users = require('./controllers/users.controller');
 const accounts = require('./controllers/accounts.controller');
 
 router.use('/auth', auth);
-router.use('/admin', admin);
+router.use('/quotes', quotes);
 router.use('/orders', orders);
 router.use('/parts', parts);
 router.use('/users', users);
