5f2b20df1143bb601d656b2ba1de3f66c1a5e222
Luke Vance <lukevance90@gmail.com>
2016-09-01 11:12:48 -0600

    user can see all priced quotes

--- client/app/controllers/user/pricedQuotes.controller.js
+++ client/app/controllers/user/pricedQuotes.controller.js
@@ -2,18 +2,35 @@ function UserQuotesController (GetUsersQuotes) {
   var vm = this;
   vm.message = "this should display the user's priced quotes.";
 
-  GetUsersQuotes(3, dealWithit);
+  GetUsersQuotes(3, loadQuotes);
 
-  function dealWithit (quotes) {
+  function loadQuotes (quotes) {
     console.log(quotes);
+    // set response data to variable on view model
     vm.usersQuotes = quotes.data.orders;
+    // loop through var on view model to add functionality
     vm.usersQuotes.forEach(function(quote){
-      quote.total = 0;
+      // set switch var for show/hide details
       quote.showDetails = false;
+      quote.detailsText = "Show";
+      // set total price value for order
+      quote.total = 0;
+      // update total price by adding each part
       quote.Order.Order_Details.forEach(function(part){
         quote.total += (part.price * part.quantity);
       });
     });
+  } // end of dealWithit
+
+  // function for switching detail view for each quote
+  vm.switchDetailView = switchDetailView;
+  function switchDetailView (quote) {
+    quote.showDetails = !quote.showDetails;
+    if (quote.detailsText === 'Show') {
+      quote.detailsText = 'Hide';
+    } else if (quote.detailsText === 'Hide'){
+      quote.detailsText = 'Show';
+    }
   }
 }
 
